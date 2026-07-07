1ae12af30beb0d5c7c3502cc7c814bef372c1adf
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-07-07 11:57:24 +0530

    get request done

--- Class 7/FullStack Employee App/Frontend/myapp/src/components/EmployeeCards.jsx
+++ Class 7/FullStack Employee App/Frontend/myapp/src/components/EmployeeCards.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+import { getuser } from '../service/api'
 
 const EmployeeCards = () => {
+  const[users, setUsers] = useState([])
+  async function getUserData(){
+    try {
+      const response = await axios.get(getuser)
+      console.log(response.data.user )
+
+      setUsers(response.data.user)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getUserData()
+  },[])
+  
   return (
     <div>
         <h1>Employee System App</h1>
@@ -10,6 +30,24 @@ const EmployeeCards = () => {
             <input placeholder='Emp. Id'/><br/>
             <button>Sumbit</button>
         </form>
+
+
+        <div>
+          {
+            users.map((item,i)=>{
+              return <div key={i}>
+                <p>Name : {item.name}</p>
+                <p>Email : {item.email}</p>
+                <p>Emp. Id: {item.empId}</p>
+                <div>
+                  <button>Delete</button>
+                  <button>Edit</button>
+                </div>
+                <hr/>
+              </div>
+            })
+          }
+        </div>
     </div>
   )
 }
--- Class 7/FullStack Employee App/server/controller/userLogic.js
+++ Class 7/FullStack Employee App/server/controller/userLogic.js
@@ -49,7 +49,8 @@ const getUser  = async (req, res) =>{
 
         res.status(200).json({
             success:true,
-            user
+            user,
+            message:'data fetched successfully...'
         })
 
 
